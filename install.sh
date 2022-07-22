661c604582e7040ac7f9a1e93ca168dfe3895fec
Kaystar <71498983+Kaystar@users.noreply.github.com>
2022-07-22 15:31:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ gitPull(){
     else
         rm -rf ${QLMainPath}/jbot/*
     fi
-    cd ${QLMainPath}/repo && rm -rf gd && git clone ${daili}https://github.com/curtinlv/gd.git
+    cd ${QLMainPath}/repo && rm -rf gd && git clone ${daili}https://github.com/Kaystar/gd.git
     cp -a ${QLMainPath}/repo/gd/* ${QLMainPath}/jbot && cp -a ${QLMainPath}/jbot/conf/* ${QLMainPath}/config && cp -a ${QLMainPath}/jbot/jk_script/* ${QLMainPath}/scripts
     rm -rf ${QLMainPath}/repo/dockerbot
     mkdir ${QLMainPath}/repo/dockerbot && ln -sf ${QLMainPath}/repo/gd ${QLMainPath}/repo/dockerbot/jbot && ln -sf ${QLMainPath}/repo/gd/conf ${QLMainPath}/repo/dockerbot/config
@@ -56,7 +56,7 @@ echo
 echo -e "\n\t\t\t【青龙安装Bot监控】\n"
 echo
 if [ -f ${QLMainPath}/jbot/user/user.py ];then
-    echo -e "\n你已部署，请启动即可:\ncd ${QLMainPath}\npython3 -m jbot\n\n或参考本仓库第3-4步:\nhttps://github.com/curtinlv/gd/blob/main/README.md"
+    echo -e "\n你已部署，请启动即可:\ncd ${QLMainPath}\npython3 -m jbot\n\n或参考本仓库第3-4步:\nhttps://github.com/Kaystar/gd/blob/main/README.md"
     echo -e "如果需要重新部署，请复制以下命令执行："
     echo -e "rm -rf  ${QLMainPath}/jbot/*  &&   bash  install.sh\n"
     exit 0
@@ -64,4 +64,4 @@ fi
 install_depend
 gitPull
 echo -e "\n*******************\n所需环境已部署完成\n*******************\n"
-echo -e "请前往面板【配置文件】配置tg机器人参数，再启动机器人即可。\n参考本仓库第3-4步: https://github.com/curtinlv/gd/blob/main/README.md "
+echo -e "请前往面板【配置文件】配置tg机器人参数，再启动机器人即可。\n参考本仓库第3-4步: https://github.com/Kaystar/gd/blob/main/README.md "
